9e8e82af10fb14c36c09872e801fdd907ccbdebf
bineea <33547731+bineea@users.noreply.github.com>
2021-02-17 23:02:25 +0800

    加载配置文件

---
+++ IPersistence/src/main/java/com/tryimpl/IPersistence/io/Resources.java
@@ -0,0 +1,16 @@
+package com.tryimpl.IPersistence.io;
+
+import java.io.InputStream;
+
+public class Resources {
+
+    /**
+     * 根据配置文件路径，将配置文件加载为字节输入流，存储在内存中
+     * @param path
+     * @return
+     */
+    public static InputStream getResourceAsStream(String path) {
+        InputStream resourceAsStream = Resources.class.getClassLoader().getResourceAsStream(path);
+        return resourceAsStream;
+    }
+}
