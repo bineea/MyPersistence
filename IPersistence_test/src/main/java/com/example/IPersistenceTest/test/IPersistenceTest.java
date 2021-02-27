bba358512087f54eb02930db8d5d54da204d5461
bineea <33547731+bineea@users.noreply.github.com>
2021-02-27 23:39:18 +0800

    测试

--- IPersistence_test/src/main/java/com/example/IPersistenceTest/test/IPersistenceTest.java
+++ IPersistence_test/src/main/java/com/example/IPersistenceTest/test/IPersistenceTest.java
@@ -1,12 +1,22 @@
 package com.example.IPersistenceTest.test;
 
-import com.tryimpl.IPersistence.io.Resources;
+import com.tryimpl.IPersistence.config.XMLConfigBuilder;
 
 import java.io.InputStream;
 
 public class IPersistenceTest {
 
-    public void ipersistenceTest() {
-        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapperConfig.xml");
+    public void ipersistenceTest() throws Exception {
+        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder();
+        InputStream resourceAsStream = XMLConfigBuilder.class.getClassLoader().getResourceAsStream("sqlMapperConfig.xml");
+        if(resourceAsStream == null) {
+            throw new Exception("配置文件不存在");
+        }
+        xmlConfigBuilder.parseConfigXml(resourceAsStream);
+    }
+
+    public static void main(String[] args) throws Exception {
+        IPersistenceTest test = new IPersistenceTest();
+        test.ipersistenceTest();
     }
 }
