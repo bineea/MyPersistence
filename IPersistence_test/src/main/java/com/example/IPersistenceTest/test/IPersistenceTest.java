b8f58317774be01183adff56c7909e8e8f139133
bineea <33547731+bineea@users.noreply.github.com>
2021-02-28 18:04:21 +0800

    测试方法

--- IPersistence_test/src/main/java/com/example/IPersistenceTest/test/IPersistenceTest.java
+++ IPersistence_test/src/main/java/com/example/IPersistenceTest/test/IPersistenceTest.java
@@ -1,27 +1,36 @@
 package com.example.IPersistenceTest.test;
 
+import com.example.IPersistenceTest.dao.TestDao;
 import com.example.IPersistenceTest.pojo.Test;
 import com.tryimpl.IPersistence.config.XMLConfigBuilder;
+import com.tryimpl.IPersistence.io.Resources;
 import com.tryimpl.IPersistence.sqlSession.SqlSession;
 import com.tryimpl.IPersistence.sqlSession.SqlSessionFactory;
 import com.tryimpl.IPersistence.sqlSession.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class IPersistenceTest {
 
     public void ipersistenceTest() throws Exception {
-        XMLConfigBuilder xmlConfigBuilder = new XMLConfigBuilder();
-        InputStream resourceAsStream = XMLConfigBuilder.class.getClassLoader().getResourceAsStream("sqlMapperConfig.xml");
-        if(resourceAsStream == null) {
-            throw new Exception("配置文件不存在");
-        }
+        // 实际应用中，会获取代理对象注册为springBean
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapperConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         SqlSession sqlSession = sqlSessionFactory.openSqlSession();
+
+        TestDao testDao = sqlSession.getMapper(TestDao.class);
+
         Test test = new Test();
         test.setId(1);
-        Test one = sqlSession.findOne("sampletest.findById", test);
-        System.out.println(one);
+        System.out.println(testDao.findById(test));
+
+        List<Test> testList = testDao.findAll();
+        for(Test testFor : testList) {
+            System.out.println(testFor.getId());
+        }
     }
 
     public static void main(String[] args) throws Exception {
