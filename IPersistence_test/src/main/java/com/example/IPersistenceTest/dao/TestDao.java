ef0928af233fafc37f4d91afe9c08f79cafb92ca
bineea <33547731+bineea@users.noreply.github.com>
2021-02-28 18:04:09 +0800

    配置文件的namespace必须为dao的全限定名，配置文件方法的id必须为dao的方法名

---
+++ IPersistence_test/src/main/java/com/example/IPersistenceTest/dao/TestDao.java
@@ -0,0 +1,12 @@
+package com.example.IPersistenceTest.dao;
+
+import com.example.IPersistenceTest.pojo.Test;
+
+import java.util.List;
+
+public interface TestDao {
+
+    List<Test> findAll();
+
+    Test findById(Test test);
+}
