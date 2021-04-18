79719e9dc0fcaec5da394895b703c1edbcbbf45a
bineea <33547731+bineea@users.noreply.github.com>
2021-04-18 17:42:31 +0800

    调整测试方法

--- IPersistence_test/src/main/java/com/example/IPersistenceTest/test/IPersistenceTest.java
+++ IPersistence_test/src/main/java/com/example/IPersistenceTest/test/IPersistenceTest.java
@@ -2,7 +2,6 @@ package com.example.IPersistenceTest.test;
 
 import com.example.IPersistenceTest.dao.TestDao;
 import com.example.IPersistenceTest.pojo.Test;
-import com.tryimpl.IPersistence.config.XMLConfigBuilder;
 import com.tryimpl.IPersistence.io.Resources;
 import com.tryimpl.IPersistence.sqlSession.SqlSession;
 import com.tryimpl.IPersistence.sqlSession.SqlSessionFactory;
@@ -10,27 +9,49 @@ import com.tryimpl.IPersistence.sqlSession.SqlSessionFactoryBuilder;
 
 import java.io.InputStream;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class IPersistenceTest {
 
-    public void ipersistenceTest() throws Exception {
+    public SqlSession openSession() throws Exception {
         // 实际应用中，会获取代理对象注册为springBean
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapperConfig.xml");
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         SqlSession sqlSession = sqlSessionFactory.openSqlSession();
+        return sqlSession;
+    }
 
-        TestDao testDao = sqlSession.getMapper(TestDao.class);
+    //查询
+    public void queryTest() throws Exception {
+        TestDao testDao = this.openSession().getMapper(TestDao.class);
+
+        List<Test> testList = testDao.findAll();
+        for(Test testFor : testList) {
+            System.out.println("查询操作："+testFor.toString());
+        }
+    }
+
+    //更新
+    public void updateTest() throws Exception {
+        TestDao testDao = this.openSession().getMapper(TestDao.class);
 
         Test test2Update = new Test();
         test2Update.setId(1L);
         test2Update.setValue(1);
         test2Update.setRemark("20210418-001");
         System.out.println("更新操作："+testDao.updateById(test2Update));
+    }
 
-        System.out.println("删除操作："+testDao.deleteById(4L));
+    //删除
+    public void deleteTest() throws Exception {
+        TestDao testDao = this.openSession().getMapper(TestDao.class);
+
+        System.out.println("删除操作："+testDao.deleteById(5L));
+    }
+
+    //插入
+    public void insertTest() throws Exception {
+        TestDao testDao = this.openSession().getMapper(TestDao.class);
 
         Test test2Insert = new Test();
         test2Insert.setValue(10);
@@ -40,14 +61,14 @@ public class IPersistenceTest {
 
         System.out.println("插入操作："+testDao.insert(test2Insert));
 
-        List<Test> testList = testDao.findAll();
-        for(Test testFor : testList) {
-            System.out.println("查询操作："+testFor.toString());
-        }
     }
 
     public static void main(String[] args) throws Exception {
         IPersistenceTest test = new IPersistenceTest();
-        test.ipersistenceTest();
+        test.queryTest();
+        test.updateTest();
+        test.deleteTest();
+        test.insertTest();
+        test.queryTest();
     }
 }
